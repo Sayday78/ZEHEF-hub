67e1bd87ac53a17991cc105132f903c8b3247dc1
Jveuxt0num9k <153019021+Sayday78@users.noreply.github.com>
2026-01-14 15:09:11 +0100

    Add AccentColor to window theme settings
    Added AccentColor property to the window configuration.

--- main.lua
+++ main.lua
@@ -33,7 +33,9 @@ local Window = Rayfield:CreateWindow({
     Name = "ZEHEF HUB  |  discord.gg/fsQ2B4tD",
     LoadingTitle = "ZEHEF HUB",
     LoadingSubtitle = "by Zehef",
-    Theme = "Dark"
+    Theme = "Dark",
+AccentColor = Color3.fromRGB(255, 105, 180) -- Rose
+
 })
 
 local MainTab = Window:CreateTab("MAIN")
